524c491446a5e4cd2b64050e0b995bfe67ea25d6
simao.lopes98 <simaolopes98@gmail.com>
2026-02-21 19:12:45 +0000

    Add ztos files

---
+++ ZTOS/include/ztos.h
@@ -0,0 +1,21 @@
+/* ztos.h */
+#ifndef ZTOS_H
+#define ZTOS_H
+
+/* Includes */
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+/* Defines */
+
+/* Type definitions */
+
+/* Private variables */
+
+/* Private function prototypes */
+
+/* Public functions */
+
+
+#endif /* ZTOS_H */
---
+++ ZTOS/src/ztos.c
@@ -0,0 +1,17 @@
+/*
+ * ztos.c
+ * ZTOS Real-Time Operating System Implementation
+ */
+
+/* Includes */
+#include "ztos.h"
+
+/* Defines */
+
+/* Type definitions */
+
+/* Private variables */
+
+/* Private function prototypes */
+
+/* Public functions */
